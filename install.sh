11321d0f9ebda5c52a00f7741e1a3090e91dc701
IriKa <qiujie.jq@gmail.com>
2020-05-06 12:34:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,19 @@
 user_id=$(id -u)
 if [ "${user_id}" != 0 ]; then
     echo "The script needs to be executed by the root user."
-    sudo $0 "$@"
-    exit $?
+    exit 1
 fi
 
 DIR=$(dirname $(realpath ${0}))
 DIR_SSTP=/etc/ss-tproxy
 
-ln -sf ${DIR}/ss-tproxy /usr/local/bin/ss-tproxy
+ln -sf ${DIR}/ss-tproxy /usr/local/bin/
 mkdir -p ${DIR_SSTP}
-ln -sf ${DIR}/chnroute6.set ${DIR_SSTP}/chnroute6.set
-ln -sf ${DIR}/chnroute.set ${DIR_SSTP}/chnroute.set
-ln -sf ${DIR}/gfwlist.ext ${DIR_SSTP}/gfwlist.ext
-ln -sf ${DIR}/gfwlist.txt ${DIR_SSTP}/gfwlist.txt
-ln -sf ${DIR}/ignlist.ext ${DIR_SSTP}/ignlist.ext
-cp -uf ${DIR}/ss-tproxy.service /etc/systemd/system/ss-tproxy.service
+ln -sf ${DIR}/chnroute6.set ${DIR_SSTP}/
+ln -sf ${DIR}/chnroute.set ${DIR_SSTP}/
+ln -sf ${DIR}/gfwlist.ext ${DIR_SSTP}/
+ln -sf ${DIR}/gfwlist.txt ${DIR_SSTP}/
+ln -sf ${DIR}/ignlist.ext ${DIR_SSTP}/
+cp -uf ${DIR}/ss-tproxy.service /etc/systemd/system/
 systemctl daemon-reload
 echo "Please link a config file for ${DIR_SSTP}/ss-tproxy.conf"
-
-exit 0
-
