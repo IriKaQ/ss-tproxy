6d87e61b68c0d012c0ab9b95330b498b659ee275
IriKa <qiujie.jq@gmail.com>
2020-05-07 09:44:22 +0800

    Update install.sh
    Signed-off-by: IriKa <qiujie.jq@gmail.com>

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ fi
 DIR=$(dirname $(realpath ${0}))
 DIR_SSTP=/etc/ss-tproxy
 
-ln -sf ${DIR}/ss-tproxy /usr/local/bin/
+cp -uf ${DIR}/ss-tproxy /usr/local/bin/
 mkdir -p ${DIR_SSTP}
-ln -sf ${DIR}/chnroute6.set ${DIR_SSTP}/
-ln -sf ${DIR}/chnroute.set ${DIR_SSTP}/
-ln -sf ${DIR}/gfwlist.ext ${DIR_SSTP}/
-ln -sf ${DIR}/gfwlist.txt ${DIR_SSTP}/
-ln -sf ${DIR}/ignlist.ext ${DIR_SSTP}/
+cp -uf ${DIR}/chnroute6.set ${DIR_SSTP}/
+cp -uf ${DIR}/chnroute.set ${DIR_SSTP}/
+cp -uf ${DIR}/gfwlist.ext ${DIR_SSTP}/
+cp -uf ${DIR}/gfwlist.txt ${DIR_SSTP}/
+cp -uf ${DIR}/ignlist.ext ${DIR_SSTP}/
 cp -uf ${DIR}/ss-tproxy.service /etc/systemd/system/
 systemctl daemon-reload
 echo "Please link a config file for ${DIR_SSTP}/ss-tproxy.conf"
